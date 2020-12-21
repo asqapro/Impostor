2a64bcc7b8092b417e0ec512d2346bdd87e19531
Adam Whisnant <asqapro11@gmail.com>
2020-12-21 03:42:47 -0600

    Improving logged info

--- src/Impostor.Server/Net/Client.cs
+++ src/Impostor.Server/Net/Client.cs
@@ -156,11 +156,13 @@ namespace Impostor.Server.Net
                         //var verified = await Player.Game.HandleGameDataAsync(readerCopy, Player, toPlayer);
                         //var verified = await Player.Game.HandleGameDataAsync(reader, Player, toPlayer);
                         var msg = BitConverter.ToString(reader.Buffer).Replace("-","");
-                        _logger.LogInformation($"Pre=edit " + msg);
+                        _logger.LogInformation($"Pre-edit " + msg);
+                        _logger.LogInformation($"Pre-edit length " + msg.Length);
 
                         reader = await Player.Game.HandleGameDataAsync(reader, Player, toPlayer);
                         
                         msg = BitConverter.ToString(reader.Buffer).Replace("-","");
+                        _logger.LogInformation($"Post-edit length " + msg.Length);
                         _logger.LogInformation($"Post-edit " + msg);
                         //if (verified != null)
                         if(reader != null)
