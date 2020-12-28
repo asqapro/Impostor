eb78052c544cc268e673e3e51d79026b64fca45f
Adam Whisnant <asqapro11@gmail.com>
2020-12-28 00:59:19 -0600

    Added movement tracking plugin. Early tests

---
+++ src/Impostor.Plugins.TrackMovement/TrackMovementPluginStartup.cs
@@ -0,0 +1,22 @@
+﻿using Impostor.Api.Events;
+using Impostor.Api.Plugins;
+using Impostor.Plugins.TrackMovement.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Impostor.Plugins.TrackMovement
+{
+    public class TrackMovementPluginStartup : IPluginStartup
+    {
+        public void ConfigureHost(IHostBuilder host)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IEventListener, GameEventListener>();
+            services.AddSingleton<IEventListener, PlayerEventListener>();
+            services.AddSingleton<IEventListener, MeetingEventListener>();
+        }
+    }
+}
